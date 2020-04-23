eb02d02cfa021187120af6a88b8045eff3c157f2
maxsynnott <maxryansynnott@gmail.com>
2020-04-23 18:01:56 +1200

    improved seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "\n"
+
+puts "Let the seeding begin"
+
+puts "\n"
 
 num_products = 10
 
@@ -15,3 +20,28 @@ num_products.times do
 
 	puts "Created #{product_name}."
 end
+
+puts "\n"
+
+email = "admin@example.com"
+password = "123456"
+
+admin = User.create(email: email, password: password)
+
+puts "Admin account created with:"
+puts "email: #{email}"
+puts "password: #{password}"
+
+puts "\n"
+
+cart = Cart.create(user: admin)
+
+puts "Admin cart created."
+
+num_orders = rand(2..5)
+
+num_orders.times do
+	Order.create(product_id: Product.all.sample.id, cart_id: cart.id, quantity: rand(1..10000))
+end
+
+puts "Admin cart populated with #{num_orders} orders."
