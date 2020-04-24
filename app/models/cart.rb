801793b4c700be5e40ede34c6015337832515d26
maxsynnott <maxryansynnott@gmail.com>
2020-04-24 12:30:14 +1200

    basic cart logic in

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,34 @@ class Cart < ApplicationRecord
   belongs_to :user
 
   has_many :orders
+
+  def add_product(product, amount = 1)
+  	order = find_order(product)
+
+  	if order
+  		order.add(amount)
+  	else
+  		Order.create(product_id: product.id, cart_id: id, quantity: amount)
+  	end
+  end
+
+  def remove_product(product, amount = nil)
+  	order = find_order(product)
+
+  	if order
+  		amount ? order.remove(amount) : order.destroy
+  	end
+  end
+
+  def quantity_of(product)
+  	find_order(product).try(:quantity)
+  end
+
+  def clear
+  	orders.destroy_all
+  end
+
+  def find_order(product)
+  	orders.find { |order| order.product_id == product.id }
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  def add(amount)
+  	update(quantity: quantity + amount)
+  end
+
+  def remove(amount)
+  	amount >= quantity ? destroy : update(quantity: quantity - amount)
+  end
 end
