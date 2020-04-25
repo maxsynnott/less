ee814203f4f21d37cbe946fffab542fd5aed1946
maxsynnott <maxryansynnott@gmail.com>
2020-04-25 18:29:41 +1200

    added user reference to addressses and created address controller

---
+++ db/migrate/20200425062810_add_user_reference_to_addresses.rb
@@ -0,0 +1,5 @@
+class AddUserReferenceToAddresses < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :addresses, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_062337) do
+ActiveRecord::Schema.define(version: 2020_04_25_062810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_04_25_062337) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "notes"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -72,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_062337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
