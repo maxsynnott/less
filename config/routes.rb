68155edb46be7f143bf2cb5d4d98271c2c19aa71
maxsynnott <maxryansynnott@gmail.com>
2020-04-25 14:17:31 +1200

    fixed routes error

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :products, only: [:index, :show]
-  resources :orders, only: [:create, :update, :destroy]
+  resources :cart_items, only: [:create, :update, :destroy]
   resources :carts, only: [:show]
 
   get 'cart/:id/checkout', to: 'carts#checkout', as: 'cart_checkout'
