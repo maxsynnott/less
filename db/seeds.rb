771579930887e4265f91f0ed84fd23eff48373fe
maxsynnott <maxryansynnott@gmail.com>
2020-05-05 11:04:40 +0200

    added address creation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-puts "\n"
-
 puts "Let the seeding begin"
-
-puts "\n"
 
 num_products = 10
 
-puts "Creating #{num_products} products:"
 num_products.times do
-	product = Product.create(name: Faker::Food.unique.ingredient, description: Faker::Food.description, price: rand(0.001..0.1))
-
-	puts "Created #{product.name}."
+	Product.create(
+		name: Faker::Food.unique.ingredient,
+		description: Faker::Food.description,
+		price: rand(0.001..0.1)
+	)
 end
 
-puts "\n"
+puts "Created #{num_products} products"
 
 email = "admin@example.com"
 password = "123456"
@@ -31,11 +21,20 @@ puts "Admin account created with:"
 puts "email: #{email}"
 puts "password: #{password}"
 
-puts "\n"
+admin.addresses.create(
+	street: "Rudi-Dutschke-Straße",
+	house_number: "26",
+	recipient: "Mr. Admin",
+	postal_code: "10969",
+	city: "Berlin",
+	state: "Berlin",
+	country: "DE",
+	notes: "Knock thrice"
+)
+
+puts "Created Admin address"
 
 cart = Cart.create(user: admin)
-
-puts "Admin cart created."
 
 num_orders = rand(2..5)
 
@@ -43,14 +42,10 @@ products = Product.all.sample(num_orders)
 
 products.each { |product| cart.add_product(product, rand(1..3000)) }
 
-puts "Admin cart populated with #{num_orders} orders."
+puts "Admin cart created and populated with #{num_orders} orders."
 
-puts "\n"
+container_sizes = [500, 1000, 1500]
 
-puts "Generating containers"
+container_sizes.each { |size| Container.create(name: "#{size.to_s}g container", size: size) }
 
-[500, 1000, 1500].each do |i|
-	container = Container.create(name: "#{i.to_s}g container", size: i)
-
-	puts "Created #{container.name}"
-end
+puts "Generated #{container_sizes.length} containers"
