4b290e67c6edad77672045f5fd168f24ab3e2eb3
maxsynnott <maxryansynnott@gmail.com>
2020-05-06 14:18:21 +0200

    recipe.add_to_cart

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_items
 
   default_scope { where(public: true) }
+
+  def add_to_cart(cart)
+  	recipe_items.each { |recipe_item| cart.add_product(recipe_item.product, recipe_item.quantity) }
+  end
 end
