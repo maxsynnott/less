1bcfa7ee5af0463a17abb6d2df1453fe4cbd2005
maxsynnott <maxryansynnott@gmail.com>
2020-05-07 18:02:07 +0200

    admin_logs_in_spec.rb

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :admin_user do
-    
+    password { "password" }
+
+    sequence :email do |n|
+      "admin#{n}@example.com"
+    end
   end
 end
---
+++ spec/features/admin/admin_logs_in_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Admin logs in" do
+	scenario "they see the admin dashboard" do
+		create(:admin_user, email: "admin@example.com", password: "password")
+
+		visit admin_root_path
+
+		fill_in "Email", with: "admin@example.com"
+		fill_in "Password", with: "password"
+
+		click_on "Login"
+
+		expect(page).to have_content("Signed in successfully")
+		expect(page).to have_content("Dashboard")
+
+		click_on "Products"
+	end
+end
