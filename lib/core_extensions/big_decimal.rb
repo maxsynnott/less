8979c05ffee1ded840f4badf961ce10b05a5622f
maxsynnott <maxryansynnott@gmail.com>
2020-05-08 15:27:16 +0200

    updated big_decimal#to_cents to round non-zeros correctly

--- lib/core_extensions/big_decimal.rb
+++ lib/core_extensions/big_decimal.rb
@@ -1,5 +1,11 @@
 class BigDecimal
 	def to_cents
-		(self * 100).ceil
+		if self.zero?
+			0
+		else
+			# All non-zero big_decimal values will return non-zero integers
+			# This is to prevent potential exploits (extremely small values will round up to 1 cent instead of down to zero) 
+			self.positive? ? [1, (self * 100).round].max : [-1, (self * 100).round].min
+		end
 	end
 end
