5fa8e7e62d232dcf84f03ca60b07b5ae37f503a6
maxsynnott <maxryansynnott@gmail.com>
2020-05-08 15:26:19 +0200

    changed wording on integer_spec

--- spec/core_extensions/integer_spec.rb
+++ spec/core_extensions/integer_spec.rb
@@ -4,33 +4,33 @@ RSpec.describe Integer, "#to_money" do
 	context "when the method is called on a positive integer" do
 		let(:integer) { Integer(2048) }
 
-		it "returns a integer divided by 100 as a BigDecimal" do
+		it "returns a integer divided by 100 as a BigDecimal instance" do
 			money = integer.to_money
 
 			expect(money).to eq 20.48
-			expect(money.class).to eq BigDecimal
+			expect(money).to be_an_instance_of BigDecimal
 		end
 	end
 
 	context "when the method is called on a negative integer" do
 		let(:integer) { Integer(-1024) }
 
-		it "returns a integer divided by 100 as a BigDecimal" do
+		it "returns a negative integer divided by 100 as a BigDecimal instance" do
 			money = integer.to_money
 
 			expect(money).to eq -10.24
-			expect(money.class).to eq BigDecimal
+			expect(money).to be_an_instance_of BigDecimal
 		end
 	end
 
 	context "when the method is called on the integer 0" do
 		let(:integer) { Integer(0) }
 
-		it "returns 0 as a BigDecimal" do
+		it "returns 0 as a BigDecimal instance" do
 			money = integer.to_money
 
 			expect(money).to eq 0
-			expect(money.class).to eq BigDecimal
+			expect(money).to be_an_instance_of BigDecimal
 		end
 	end
 end
