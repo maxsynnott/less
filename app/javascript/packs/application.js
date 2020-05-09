2486c48abeee50c69145ca51219ad2cdf9171324
maxsynnott <maxryansynnott@gmail.com>
2020-05-09 14:53:29 +0200

    installed stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,15 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// Self-added below:
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 
 import '../stylesheets/application'
 
