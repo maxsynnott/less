1207337323f238985ee284f385743c5caea19924
maxsynnott <maxryansynnott@gmail.com>
2020-05-09 16:50:15 +0200

    moved stimulus.js intialize code into app/javascripts/packs/stimulus.index.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,12 +22,7 @@ import routes from '../routes'
 
 global.routes = routes
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("./stimulus/controllers", true, /\.js.erb$/)
-application.load(definitionsFromContext(context))
+import "./stimulus"
 
 import '../stylesheets/application'
 
---
+++ app/javascript/packs/stimulus/index.js
@@ -0,0 +1,6 @@
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js.erb$/)
+application.load(definitionsFromContext(context))
