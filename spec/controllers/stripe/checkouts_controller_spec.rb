3785672677367535b54caca441a99d6d6f80105a
maxsynnott <maxryansynnott@gmail.com>
2020-05-11 15:51:19 +0200

    parsed_response

--- spec/controllers/stripe/checkouts_controller_spec.rb
+++ spec/controllers/stripe/checkouts_controller_spec.rb
@@ -12,10 +12,12 @@ RSpec.describe Stripe::CheckoutsController, "#new" do
 		it "renders a JSON with a session id" do
 			get :new
 
+			parsed_response = JSON.parse(response.body)
+
 			# stripe-ruby-mock returns the session_id: "test_cs_1"
 			# However in reality stripe seems to generate ids in format "cs_test_...."
 			# The following regex: /(cs_|test_){2}\w+/ should match either format
-			expect(JSON.parse(response.body)["session_id"]).to match /(cs_|test_){2}\w+/
+			expect(parsed_response["session_id"]).to match /(cs_|test_){2}\w+/
 		end
 	end
 
@@ -23,7 +25,9 @@ RSpec.describe Stripe::CheckoutsController, "#new" do
 		it "renders a JSON with an error message" do
 			get :new
 
-			expect(JSON.parse(response.body)["error"]).to eq "Cannot checkout with an empty cart"
+			parsed_response = JSON.parse(response.body)
+
+			expect(parsed_response["error"]).to eq "Cannot checkout with an empty cart"
 		end
 	end
 end
