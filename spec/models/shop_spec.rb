79c29ac3289c22977fd95ba24d7c3d9d39143b23
maxsynnott <maxryansynnott@gmail.com>
2020-06-02 22:47:32 +0200

    all model specs passing

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Shop, 'validations' do
+	[]
+	.each { |attr| it { is_expected.to validate_presence_of(attr) } }
 end
