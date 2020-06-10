80f22e8533f5d819b27d39e58f08b1ae4ff89e71
maxsynnott <maxryansynnott@gmail.com>
2020-06-10 14:47:23 +0200

    changed email address

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
 	def new_order_email
 		@order = params[:order]
 
-		mail(to: "max@lessberlin.com", subject: "You've got a new order!")
+		mail(to: "max@lessberlin.de", subject: "You've got a new order!")
 	end
 end
