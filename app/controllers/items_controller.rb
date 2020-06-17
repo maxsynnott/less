7852b3f3d7951e21b7db8c079f8ac92f349b6046
maxsynnott <maxryansynnott@gmail.com>
2020-06-17 12:21:30 +0200

    progress on item show modal

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:index]
+	skip_before_action :authenticate_user!, only: [:index, :show, :autocomplete]
 
   def index
     @items = Item.all
