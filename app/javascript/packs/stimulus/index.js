1e4b0bbdf9b1d5a7f145c980d6efc7d820a134fd
maxsynnott <maxryansynnott@gmail.com>
2020-06-18 20:13:02 +0200

    cart sidebar progress

--- app/javascript/packs/stimulus/index.js
+++ app/javascript/packs/stimulus/index.js
@@ -3,4 +3,5 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js.erb$/)
+
 application.load(definitionsFromContext(context))
