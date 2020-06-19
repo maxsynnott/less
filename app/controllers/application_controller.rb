c57262fa74a92d7d90c3190c3d629cbf65422de9
maxsynnott <maxryansynnott@gmail.com>
2020-06-19 18:35:17 +0200

    made locale persistant across sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def set_locale
-		locale = params[:locale].to_s.to_sym
-
-	  I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
+		I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+		session[:locale] = I18n.locale
 	end
 end
