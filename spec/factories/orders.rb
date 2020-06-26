7873fbdd264951ea82274f55e5f60708e32e1646
maxsynnott <maxryansynnott@gmail.com>
2020-06-26 17:12:20 +0200

    test fix

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :order do
+  	payment_method_id { "pm_0123456789" }
+
   	# Handling has_one association
     after(:build) do |order, evaluator|
       create(:delivery, order: order)
